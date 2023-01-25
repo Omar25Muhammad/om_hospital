cd3e1ed85cbfc12711f886aa1a196fb06265ae86
Omar Muhammad <maitreomar25@yahoo.com>
2023-01-25 13:27:44 +0200

    Extras

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-     'name': 'Hospital Management',
-     'version': '1.03',
-     'summary': 'Hospital Managament App',
-     'sequence': 1,
-     'description': """An app for managing hosptial services""",
-     'category': 'Services',
-     'website': 'https:/www.google.com',
-     'images': [],
-     'depends': [],
-     'data': 
-         [
+    'name': 'Hospital Management',
+    'version': '1.03',
+    'summary': 'Hospital Managament App',
+    'sequence': 1,
+    'description': """An app for managing hosptial services""",
+    'category': 'Services',
+    'website': 'https:/www.google.com',
+    'images': [],
+    'depends': [
+        'mail'
+    ],
+    'data':
+        [
             'security/ir.model.access.csv',
             'views/patient.xml'
-            ],
-     'demo': [],
-     'qweb': [],
-     'installable': True,
-     'application': True,
-     'auto_install': True,
-     'license': 'LGPL-3',
- }
+        ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
--- models/patient.py
+++ models/patient.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
+from odoo import fields, models
 
-from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Information about the patients'
-    
+
     # Now we add the fields
-    first_name = fields.Char(string='First Name', required=True)
+    name = fields.Char(string='First Name', required=True)
     last_name = fields.Char(string='Last Name', required=False)
     age = fields.Integer(string='Age', required=True)
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female')
-    ])
+    ], required=True, default='male')
     notes = fields.Text(string='Notes')
-    
-    
